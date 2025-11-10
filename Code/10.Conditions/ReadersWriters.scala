4665f3629504c44bcd0bd5567f21e378c70bf5c9
Gavin Lowe <gavin.lowe@cs.ox.ac.uk>
2025-11-10 09:27:33 +0000

    Added readers and writers to the conditions chapter.

---
+++ Code/10.Conditions/ReadersWriters.scala
@@ -0,0 +1,106 @@
+package tacp.monitors
+
+import ox.scl._
+
+/** Trait for the readers and writers problem. */
+trait ReadersWritersLock{
+  /** A reader enters the critical section. */
+  def readerEnter(): Unit
+  /** A reader exits the critical section. */
+  def readerExit(): Unit
+  /** A writer enters the critical section. */
+  def writerEnter(): Unit
+  /** A writer exits the critical section. */
+  def writerExit(): Unit
+}
+
+// =======================================================
+
+class MonitorReadersWriters0 extends ReadersWritersLock{
+  /** Number of readers currently in the critical region. */
+  private var readers = 0
+
+  /** Number of writers currently in the critical region. */
+  private var writers = 0
+
+  /* Invariant: (readers = 0 || writers = 0) && writers <= 1. */
+
+  private val lock = new Lock
+
+  private val cond = lock.newCondition
+
+  def readerEnter() = lock.mutex{
+    cond.await(writers == 0)
+    readers += 1
+  }
+
+  def readerExit() = lock.mutex{
+    assert(writers == 0); readers -= 1; cond.signalAll()
+  }
+
+  def writerEnter() = lock.mutex{
+    cond.await(readers == 0 && writers == 0)
+    writers += 1
+  }
+
+  def writerExit() = lock.mutex{
+    assert(writers == 1 && readers == 0); writers -= 1; cond.signalAll()
+  }
+}
+
+
+// =======================================================
+
+/** A solution to the readers and writers problem using an SCL monitor. */
+class MonitorReadersWriters extends ReadersWritersLock{
+  /** Number of readers currently in the critical region. */
+  private var readers = 0
+
+  /** Number of writers currently in the critical region. */
+  private var writers = 0
+
+  /* Invariant: (readers = 0 || writers = 0) && writers <= 1. */
+
+  /** Number of readers waiting to enter. */
+  private var readersWaiting = 0
+
+  /** Number of writers waiting to enter. */
+  private var writersWaiting = 0
+
+  private val lock = new Lock
+
+  /** Condition to signal to a waiting reader that it can now enter. */
+  private val readerEnterC = lock.newCondition
+
+  /** Condition to signal to a waiting writer that it can now enter. */
+  private val writerEnterC = lock.newCondition
+
+  def readerEnter() = lock.mutex{
+    if(writers > 0 || writersWaiting > 0){ // Have to wait
+      readersWaiting += 1
+      readerEnterC.await(writers == 0)
+      readersWaiting -= 1
+    }
+    readers += 1
+  }
+
+  def readerExit() = lock.mutex{
+    assert(writers == 0); readers -= 1 
+    if(readers == 0 && writersWaiting > 0) writerEnterC.signal()
+  }
+
+  def writerEnter() = lock.mutex{
+    if(readers > 0 || writers > 0){ // Have to wait
+      writersWaiting += 1
+      writerEnterC.await(readers == 0 && writers == 0)
+      writersWaiting -= 1
+    }
+    writers += 1
+  }
+
+  def writerExit() = lock.mutex{
+    assert(writers == 1 && readers == 0); writers -= 1 
+    if(readersWaiting > 0) readerEnterC.signalAll()
+    else if(writersWaiting > 0) writerEnterC.signal()
+  }
+}
---
+++ Code/10.Conditions/ReadersWritersTest.scala
@@ -0,0 +1,75 @@
+package tacp.monitors
+
+import ox.scl._
+
+/** A linearizability tester for the readers and writers problem. */
+object ReadersWritersTest{
+  var iters = 200  // Number of iterations by each worker.
+  val p = 6 // Number of workers.
+
+  /** The type of concurrent objects being tested. */
+  type CRW = ReadersWritersLock
+
+  /** The type of sequential specification objects, recording the number of
+    * readers and writers currently in the critical region. */
+  case class SRW(readers: Int, writers: Int)
+
+  def seqReaderEnter(s: SRW): (Unit, SRW) = {
+    val SRW(r,w) = s; require(w == 0); ((), SRW(r+1, w))
+  }
+
+  def seqReaderExit(s: SRW): (Unit, SRW) = {
+    val SRW(r,w) = s; assert(r > 0); ((), SRW(r-1, w))
+  }
+
+  def seqWriterEnter(s: SRW): (Unit, SRW) = {
+    val SRW(r,w) = s; require(w == 0 && r == 0); ((), SRW(0, 1))
+  }
+
+  def seqWriterExit(s: SRW): (Unit, SRW) = {
+    val SRW(r,w) = s; assert(w == 1); ((), SRW(0, 0))
+  }
+
+  def worker(me: Int, log: LinearizabilityLog[SRW, CRW]) = {
+    val random = new scala.util.Random
+    // Pause for a short while.
+    def pause() = while(random.nextInt(1000) > 0){} 
+    for(i <- 0 until iters){
+      pause()
+      if(random.nextInt(2) == 0){
+        log(_.writerEnter(), "writerEnter", seqWriterEnter)
+        pause()
+        log(_.writerExit(), "writerExit", seqWriterExit)
+      }
+      else{
+        log(_.readerEnter(), "readerEnter", seqReaderEnter)
+        pause()
+        log(_.readerExit(), "readerExit", seqReaderExit)
+      }
+    }
+  }
+
+  val MRW0 = 0; val MRW = 1
+
+  def doTest(choice: Int) = {
+    val rw: CRW = choice match{
+      case MRW0 => new MonitorReadersWriters0
+      case MRW => new MonitorReadersWriters
+    }
+    val seqRW = SRW(0,0)
+    val tester = LinearizabilityTester[SRW,CRW](seqRW, rw, p, worker _)
+    assert(tester() > 0)
+  }
+
+  def main(args: Array[String]) = {
+    var i = 0; var choice = MRW0
+    while(i < args.length) args(i) match{
+      case "--monitor0" => choice = MRW0; i += 1
+      case "--monitor" => choice = MRW; i += 1
+    }
+
+    for(i <- 0 until 1000){ doTest(choice); if(i%10 == 0) print(".") }
+    println()
+  }
+
+}
